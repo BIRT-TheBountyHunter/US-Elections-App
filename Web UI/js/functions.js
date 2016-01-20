76a15897bf8e09654d6f3eac04608ba634354636
tlowryot <tlowry@opentext.com>
2016-01-20 11:30:24 -0500

    Redesigned Share Functionality and added functions to handle click events on share icons and share button

--- Web UI/js/functions.js
+++ Web UI/js/functions.js
@@ -4,7 +4,6 @@ $(document).ready(function() {
     $.idleTimer(15000);
     $(document).bind("idle.idleTimer", function() {
 	    $('.filterPane').removeClass('filterPaneOpen');
-	    $('#sharePullDown').removeClass('shareOpen');
 	    $(this).find('span.textLabel').removeClass('hideText');
     });
 	
@@ -20,11 +19,6 @@ $(document).ready(function() {
 		iframe: 'howitworks.html',
 		iframeWidth: 800,
 		iframeHeight: 550
-	});
-	
-	// Share Button  
-	$('#menu li.share a').click(function(){
-		$('#sharePullDown').toggleClass('shareOpen');
 	});
 	
 	//Row click functionality on data tables
@@ -61,7 +55,6 @@ $(document).ready(function() {
 	window.onscroll=testScroll;
 	
 	//Profile / Topic Menu
-	
 	$('body.profile h1').click(function(){
 		$('#profileTopicMenu').addClass('displayed');
 	});
@@ -70,4 +63,114 @@ $(document).ready(function() {
 		$('#profileTopicMenu').removeClass('displayed');
 	});
 	
+	// SHARE MENU //
+	
+		//Share Menu click function
+		$('a.share').click(function(){
+			$('#menu li:nth-child(2)').toggleClass('liWidth');
+			$(this).toggleClass('shareHidden');
+			setTimeout(function(){
+			  $('a.shareIcon').addClass('showIcons');
+			}, 50);
+		});
+		
+		//Share Menu Mouse leave functions for share menu
+		var timer;
+		$('#menu li:nth-child(2)').on('mouseover', function() {
+			//clear time on mouse re-enter/over
+			clearTimeout(timer);
+		}).on('mouseleave', function() {
+			//delays closing of share menu so that it doesn't appear glitch if mouse leaves the element for a moment
+			timer = setTimeout(function(){
+		    	$('#menu li:nth-child(2)').removeClass('liWidth');
+				$('a.share').removeClass('shareHidden');
+				$('a.shareIcon').removeClass('showIcons');
+		  }, 1000);
+		});
+		
+		
+		//Handle click events for each social icon
+		
+		//TWITTER
+		$('a.twitter').click(function() {
+			
+			//Base URL to Twitter Intent
+			baseURL = "https://twitter.com/intent/tweet?";
+			
+			//Tweet text
+			tweetText = "Data analysis of US election media coverage";
+			
+			//URL to current page
+			currentURL = window.location.href;
+
+			//Hashtags
+			hashtag = "ElectionMediaAnalysis";
+			
+			window.location.href = baseURL + "url=" + currentURL + "&text=" + tweetText + "&hashtags=" + hashtag + "&source=webclient";
+		});
+		
+		//FACEBOOK
+		$('a.facebook').click(function() {
+			
+			//Base URL to Twitter Intent
+			baseURL = "https://www.facebook.com/sharer/sharer.php?";
+
+			//URL to current page
+			currentURL = window.location.href;
+
+			window.location.href = baseURL + "u=" + currentURL;
+		});
+		
+		//Google+
+		$('a.google').click(function() {
+			
+			//Base URL to Twitter Intent
+			baseURL = "https://plus.google.com/share?";
+			
+			//URL to current page
+			currentURL = window.location.href;
+			currentURLEnc = encodeURI(currentURL);
+			
+			window.location.href = baseURL + "url=" + currentURLEnc;
+		});
+		
+		//LinkedIn
+		$('a.linkedin').click(function() {
+			
+			//Base URL to Twitter Intent
+			baseURL = "https://www.linkedin.com/shareArticle?mini=true";
+			
+			//Title
+			titleText = "Data analysis of US election media coverage";
+			
+			//URL to current page
+			currentURL = window.location.href;
+			
+			window.location.href = baseURL + "&url=" + currentURL + "&title=" + titleText;
+			
+		});
+		
+		//Email
+		$('a.email').click(function() {
+			
+			//Base URL to Twitter Intent
+			baseURL = "mailto:?";
+			
+			//Subject
+			subjectText = "Data analysis of US election media coverage";
+			subjectTextEnc = encodeURI(subjectText);
+			
+			//URL to current page
+			currentURL = window.location.href;
+			currentURLEnc = encodeURI(currentURL);
+			
+			//Body
+			bodyText = "Check out this media analysis of the 2016 Presidental Election media coverage here: " + currentURLEnc;
+			bodyTextEnc = encodeURI(bodyText);
+			
+			window.location.href = baseURL + "subject=" + subjectTextEnc + "&body=" + bodyTextEnc;
+			
+		});
+		
 });
+	
